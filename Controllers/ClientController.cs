4eabfdd2569e137dcede347d5ac327c320df461b
VieiraLuan <105254554+VieiraLuan@users.noreply.github.com>
2022-07-07 12:05:21 -0300

    Estudando Fluxo e comentando código

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -10,15 +10,16 @@ namespace Fiap.Web.AspNet3.Controllers
         {/*GET*/
 
             //Simulando Busca no BD
+            //Carregar Clientes
 
             var listaClientes = new List<ClientModel>();
 
-            listaClientes.Add( new ClientModel
+            listaClientes.Add(new ClientModel
             {
                 ClientId = 1,
                 Name = "Flávio",
                 Email = "fmoreni@gmail.com",
-                Birth = "14/04/2000",
+                Birth = DateTime.Now,
                 Observation = "OBS1"
             });
 
@@ -27,7 +28,7 @@ namespace Fiap.Web.AspNet3.Controllers
                 ClientId = 2,
                 Name = "Diogo",
                 Email = "fmoreni@gmail.com",
-                Birth = "14/04/2000",
+                Birth = DateTime.Now,
                 Observation = "OBS1"
             });
 
@@ -36,7 +37,7 @@ namespace Fiap.Web.AspNet3.Controllers
                 ClientId = 3,
                 Name = "Luan",
                 Email = "fmoreni@gmail.com",
-                Birth = "14/04/2000",
+                Birth = DateTime.Now,
                 Observation = "OBS1"
             });
 
@@ -45,7 +46,7 @@ namespace Fiap.Web.AspNet3.Controllers
                 ClientId = 4,
                 Name = "Thais",
                 Email = "fmoreni@gmail.com",
-                Birth = "14/04/2000",
+                Birth = DateTime.Now,
                 Observation = "OBS1"
             });
 
@@ -54,79 +55,88 @@ namespace Fiap.Web.AspNet3.Controllers
                 ClientId = 5,
                 Name = "Fernando",
                 Email = "fmoreni@gmail.com",
-                Birth = "14/04/2000",
+                Birth = DateTime.Now,
                 Observation = "OBS1"
             });
 
+            //ViewBag.MessageObs = "Mensagem de Teste da Obs";
+            //TempData["MessageTemp"] = "Mensagem temporária";
 
-            return View(listaClientes);
-        }
+            return View(listaClientes);/*Manda para a View esse Objeto*/
+        }/*Carregar Lista de Clientes*/
 
-        [HttpGet]
+       
         public IActionResult NewClient()
         {
             return View();
-        }
+        } /*Ir para a Pagina de Cadastro de um Novo Cliente*/
 
         [HttpPost]
-        public IActionResult NewClient(ClientModel clientModel)
-        {
+        public IActionResult NewClient(ClientModel clientModel)/*Passar o Model desse Objeto*/
+        { /*View retorna Model Preenchido*/
+            if (ModelState.IsValid) /*Valida se tem algum dado faltando no Model*/
+            {
 
-            //Recuperar as informações do cliente digitado
-            /*Passar por parametro a classe e um objeto para carregar*/
+                //Cadastrar Banco (BD FAKE)
+                TempData["mensagem"] = "Cliente cadastrado com sucesso";/*Retorna Mensagem de Sucesso na View*/
 
-
-
-            //Cadastrar Banco (BD FAKE)
-            TempData["mensagem"] = "Cliente cadastrado com sucesso";
-
-            //Exibir uma tela de sucesso!
-            return RedirectToAction("Index");
+                //Exibir uma View de sucesso! Com a Mensagem acima
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //Recuperar as informações do cliente digitado na View de Cadastro
+                // processo será o mesmo até o usuario Validar ou Cancelar a Alteração
+                return View(clientModel);
+            }
         }
 
 
         [HttpGet]
-        public IActionResult Edit(int id)
-        {
-                var  clientModel = new ClientModel
-                {
-                    ClientId = 3,
-                    Name = "Moreni",
-                    Email = "moreni@gmail.com",
-                    Birth = "14/04/2000",
-                    Observation = "OBS3"
-                };
-            
-            
-            
-            return View(clientModel);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(ClientModel clientModel)
-        {
-
-            //Recuperar as informações do cliente digitado
-            /*Passar por parametro a classe e um objeto para carregar*/
-
-
-
-            //Cadastrar Banco (BD FAKE)
-
-
-            //Exibir uma tela de sucesso!
-            return RedirectToAction("Index");
-        }
-
-
-        public IActionResult Detales(int id)
+        public IActionResult Edit(int id)//Verificar com o professor
         {
             var clientModel = new ClientModel
             {
                 ClientId = 3,
                 Name = "Moreni",
                 Email = "moreni@gmail.com",
-                Birth = "14/04/2000",
+                Birth = DateTime.Now,
+                Observation = "OBS3"
+            };
+
+
+
+            return View(clientModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ClientModel clientModel)/*Passar por parametro a classe e um objeto para carregar*/
+        {
+            
+            if (ModelState.IsValid) {
+                //Editar Cliente no Banco 
+                TempData["mensagem"] = "Cliente Alterado com Sucesso"; /*Exibe Mensagem de Sucesso na View*/
+                //Exibir uma tela de sucesso!
+                
+                return RedirectToAction("Index");
+            } else {
+                //Recuperar as informações do cliente digitado na View de Cadastro
+                // processo será o mesmo até o usuario Validar ou Cancelar a Alteração
+                return View(clientModel);
+
+            }
+           
+        }
+
+
+        public IActionResult Detales(int id) //Verificar com o Professor
+        {
+            var clientModel = new ClientModel
+            {
+                ClientId = 3,
+                Name = "Moreni",
+                Email = "moreni@gmail.com",
+                Birth = DateTime.Now,
                 Observation = "OBS3"
             };
 
@@ -136,6 +146,6 @@ namespace Fiap.Web.AspNet3.Controllers
         }
 
 
-     
+
     }
 }
--- Models/ClientModel.cs
+++ Models/ClientModel.cs
@@ -1,16 +1,43 @@
-﻿namespace Fiap.Web.AspNet3.Models
+﻿using Microsoft.AspNetCore.Mvc; /*Biblioteca Padrão*/
+using System.ComponentModel.DataAnnotations;/*Biblioteca das Annotations*/
+
+namespace Fiap.Web.AspNet3.Models
 {
     public class ClientModel
     {
-        public int ClientId{ get; set; }
+        [Display(Name = "Id do Cliente")]
+        [HiddenInput] /*Aqui definimos se ele é visivel ou nao*/
+        public int ClientId { get; set; }
+
+
+        [Display(Name = "Nome")]
+        [MaxLength(70, ErrorMessage = "Tamanho máximo do nome é 70 caracteres")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [MinLength(2, ErrorMessage ="Digite um nome com mais de 2 caracteres")]
         public string Name { get; set; }
 
-        public string Email{ get; set; }
 
-        public string Birth { get; set; }
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Digite o E-mail com @")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Email { get; set; }
 
-        public string Observation { get; set; }
 
+        [Display(Name = "Data de Nascimento")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [DataType(DataType.Date,ErrorMessage ="Campo Obrigatório")]
+        public DateTime? Birth { get; set; } /*Quando definimos DataType ou EmailAdrres
+                                              automaticamente no Form os campos se tornar do mesmo type*/
+
+        /*Utilizamos ? para permitir que no html seja null, mas que 
+         no model qnd usado a anotation required seja solicitado e retornar  
+        o erro da anotation não do padrão do html*/
+
+        [Display(Name = "Observação")] /*Display name é o nome que iremos definir dentro do label
+                                        no html*/
        
+        public string? Observation { get; set; }
+
+
     }
 }
