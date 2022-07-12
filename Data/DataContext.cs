2b1c1e3155780153f96e7eeebc85680545142f09
VieiraLuan <105254554+VieiraLuan@users.noreply.github.com>
2022-07-11 22:05:07 -0300

    conectando bd

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Fiap.Web.AspNet3.Models;
+
+namespace Fiap.Web.AspNet3.Data
+{
+    public class DataContext : DbContext
+    {
+       
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<RepresentanteModel> Representantes { get; set; }
+    }
+}
