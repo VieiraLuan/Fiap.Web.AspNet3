17be37207b4b4471794128411128f86b258457f4
VieiraLuan <105254554+VieiraLuan@users.noreply.github.com>
2022-07-12 22:43:27 -0300

    Estudando estrutura

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,12 +6,17 @@ namespace Fiap.Web.AspNet3.Data
 {
     public class DataContext : DbContext
     {
-       
+
+
+        public DbSet<RepresentanteModel>Representantes { get; set; }
+        /*Propriedade*/
+
+        //public DbSet<ClientModel> Clients { get; set; } 
+
+
         public DataContext(DbContextOptions options) : base(options)
-        {
+        {/*Construtor*/
 
         }
-
-        public DbSet<RepresentanteModel> Representantes { get; set; }
     }
 }
